072918472edc3141501b4f6e6010fc6b5740d246
shraddha36 <76175909+shraddha36@users.noreply.github.com>
2021-02-03 12:47:40 +0530

    Update ArmstrongNo.py

--- ArmstrongNo.py
+++ ArmstrongNo.py
@@ -8,3 +8,20 @@ if int(a)==cube_add:
 else:
     
     print(False)
+
+"""
+a=int(input("enter no."))
+cube_add=0
+num=a
+
+while num>0:
+    c=num%10
+    cube_add+=c**3
+    num //=10
+if a==cube_add:
+    print(True)
+else:
+    print(False)
+"""
+"""153%10=3
+153//10=15"""
